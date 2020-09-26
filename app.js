9fcc9e1f6fe573e09942d6ee827e450bde2edd32
Amir Ayub <amirayubx@gmail.com>
2020-09-26 19:30:13 +0300

    save hashed user

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
+import User from './models/user.js';
 
+const connectionString = 'mongodb+srv://amir:2904@cluster0.t1yta.mongodb.net/secure-pass?retryWrites=true&w=majority';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const usernmame = 'amir';
 const pass = '2904';
-const rounds = 14;
+const rounds = 12;
 
-bcrypt.hash(pass, rounds).then((hash) => {
-    console.log(hash);
+const createUser = (hashed_username, hashed_password) => {
+    let user = new User({
+        username: hashed_username,
+        password: hashed_password
+    });
+    return user;
+}
+
+bcrypt.hash(usernmame, rounds).then((hashed_username) => {
+    bcrypt.hash(pass, rounds).then((hashed_password) => {
+        createUser(hashed_username, hashed_password).save().then((model) => {
+            console.log(model);
+        });
+    });
 });
---
+++ models/user.js
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+const User = mongoose.model('User', {
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+export default User;
