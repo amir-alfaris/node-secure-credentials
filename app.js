0279bd17a08a397ed449862b66736e74d23e6d4a
Amir Ayub <amir@Amirs-MacBook-Pro-16.local>
2020-10-14 22:13:47 +0300

    added login router

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import body_parser from 'body-parser';
 
 import create_user_router from './routes/create-user.js';
 import list_user_router from './routes/list-user.js';
+import login_router from './routes/login.js';
 
 const connectionString = 'mongodb+srv://amir:2904@cluster0.t1yta.mongodb.net/secure-pass?retryWrites=true&w=majority';
 const port = 3000;
@@ -19,6 +20,7 @@ mongoose.connect(connectionString, {
 app.use(body_parser.json());
 app.use('/create', create_user_router);
 app.use('/list', list_user_router);
+app.use('/login', login_router);
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
--- routes/login.js
+++ routes/login.js
@@ -1,12 +1,20 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
+import sha from 'js-sha3';
 
 import User from '../models/user.js';
 
 const router = express.Router();
 
 router.post('/', (req, res) => {
-
+    User.findOne({
+        username: sha.keccak256(req.body.username)
+    }).then((err, user) => {
+        if (user) {
+            res.send('user present');
+        } else {
+            res.send('user does not exist');
+        }
+    });
 });
 
 export default router;
